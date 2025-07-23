664da58f91c9e551e32776e6e4ad962efb376f74
nurik-12 <turakuzieva@gmail.com>
2025-07-23 08:25:58 +0500

    Add files via upload

---
+++ js.js
@@ -0,0 +1,12 @@
+let button = document.querySelector(".img");
+let btm = document.querySelector(".image");
+let box = document.querySelector(".box");
+
+button.addEventListener("click" ,function(e){
+window.location.href = "index.html";
+})
+button.addEventListener("click" ,() => {
+if(
+    box
+)
+    })
